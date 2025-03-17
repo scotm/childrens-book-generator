1855d72c06ef11e04d5994c5747fc4eb26e31058
Scott Macdonald <scott.scotm@gmail.com>
2025-03-17 20:57:29 +0000

    Add UploadThing API integration with file URL retrieval function

---
+++ src/server/uploadthing.ts
@@ -0,0 +1,22 @@
+import { env } from "@/env.mjs";
+import { UTApi } from "uploadthing/server";
+
+export const utapi = new UTApi({
+  token: env.UPLOADTHING_TOKEN,
+});
+
+type UploadThingFileKey = Awaited<
+  ReturnType<typeof utapi.listFiles>
+>["files"][number]["key"];
+
+/**
+ * Retrieves the URL of a file stored in UploadThing.
+ *
+ * @param {UploadThingFileKey} fileKey - The unique key associated with the file.
+ * @returns {Promise<string>} A promise that resolves to the URL of the file.
+ */
+export const getFileUrl = async (
+  fileKey: UploadThingFileKey
+): Promise<string> => {
+  return `https://${env.UPLOADTHING_APP_ID}.ufs.sh/f/${fileKey}`;
+};
